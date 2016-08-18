9e84a3b9296e2b04fa027454dc9ea50bece09e9b
David Pearson <Entropy@ksu-wireless-228-177.campus.ksu.edu>
2016-08-18 15:53:06 -0500

    Adding some files and updating progress

--- MCMC/MCMC.cpp
+++ MCMC/MCMC.cpp
@@ -3,5 +3,40 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <
+#include <cmath>
+#include <omp.h>
+#include <harppi.h>
+#include <libmcmc.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_permutation.h>
+#include "model.h"
 
+std::string filename(std::string filebase, int digits, int filenum, std::string fileext) {
+    std::stringstream file;
+    file << filebase << std::setw(digits) << std::setfill('0') << filenum << fileext;
+    return file.str();
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        std::cout << "ERROR: Expected parameter file.\n";
+        std::cout << "       Usage: " << argv[0] << " ParameterFile.params\n";
+        std::cout << "       Note that the parameter file can have any extension\n";
+        std::cout << "       so long as it is a plain text file formated as specified\n";
+        std::cout << "       libharppi documentation." << std::endl;
+        return 0;
+    }
+    parameters p;
+    p.readParams(argv[1]);
+    
+    std::ifstream fin;
+    std::ofstream fout;
+    
+    std::vector< double > modparams;
+    for (int i = 0; i < p.geti["numParams"]; ++i) {
+        modparams.push_back(p.getd("paramVals", i));
+    }
