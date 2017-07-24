865b4ce86e4c051d56d585bdc08233561528b655
David Pearson <dpearson1983@gmail.com>
2017-07-24 13:34:46 -0500

    Modify bkmcmc6.h for smoothing parameters

--- MCMCFits/main.cpp
+++ MCMCFits/main.cpp
@@ -4,27 +4,60 @@
 #include <string>
 #include <vector>
 #include <CCfits/CCfits>
-#include "include/harppi.h"
+#include <harppi.h>
 
 int main(int argc, char *argv[]) {
-    parameter p(argv[1]);
+    parameters p(argv[1]);
     p.print();
     
-    std::unique_ptr<FITS> pFits(0);
-    pFits.reset(new FITS(p.gets("out_file"), Write));
+    std::ifstream fin;
+    
+    std::unique_ptr<CCfits::FITS> pFits;
+    pFits.reset(new CCfits::FITS(p.gets("out_file"), CCfits::Write));
     
     std::string hduName("MCMC_CHAIN");
     
     std::vector<std::string> col_name(p.geti("num_cols"));
     std::vector<std::string> col_form(p.geti("num_cols"));
-    std::vector<std::string> col_unit(p.geti("num_cols"));
+//     std::vector<std::string> col_unit(p.geti("num_cols"));
     
+    std::cout << "Getting column info from parameters..." << std::endl;
     for (int i = 0; i < p.geti("num_cols"); ++i) {
         col_name[i] = p.gets("col_name", i);
         col_form[i] = p.gets("col_form", i);
-        col_unit[i] = p.gets("col_unit", i);
+//         col_unit[i] = p.gets("col_unit", i);
     }
     
-    Table *newTable = pFits->addTable(hduName, rows, col_name, col_form, col_unit);
+    std::vector<std::vector<double>> data(p.geti("num_cols"));
     
+    std::cout << "Reading in the data file..." << std::endl;
+    if (std::ifstream(p.gets("in_file"))) {
+        fin.open(p.gets("in_file").c_str(), std::ios::in);
+        while (!fin.eof()) {
+            for (int i = 0; i < p.geti("num_cols"); ++i) {
+                double temp;
+                fin >> temp;
+                if (!fin.eof()) {
+                    data[i].push_back(temp);
+                }
+            }
+        }
+        fin.close();
+    } else {
+        std::stringstream message;
+        message << " Could not open " << p.gets("in_file") << std::endl;
+        throw std::runtime_error(message.str());
+    }
     
+    int rows = data[0].size();
+    std::cout << "rows = " << rows << std::endl;
+    
+    CCfits::Table *newTable = pFits->addTable(hduName, rows, col_name, col_form);
+    
+    std::cout << "Writing fits file..." << std::endl;
+    for (int i = 0; i < p.geti("num_cols"); ++i) {
+        newTable->column(col_name[i]).write(data[i], 1);
+    }
+    
+    return 0;
+}
