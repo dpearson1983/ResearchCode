a8e67222eb2d72cad392af06ac660c62400bc34f
David Pearson <dpearson1983@gmail.com>
2017-08-22 11:52:54 -0500

    Adding version 11 of bkMCMC

---
+++ bkMCMC/v11/make_spline.h
@@ -0,0 +1,10 @@
+#ifndef _MAKE_SPLINE_H_
+#define _MAKE_SPLINE_H_
+
+#include <vector>
+#include <string>
+#include <vector_types.h>
+
+std::vector<float4> make_spline(std::string in_pk_file);
+
+#endif
