9ae0a59c7ce1c6f5ca4e5d4d9610f5add869b1bf
David Pearson <Entropy@guest-wireless-216-174.campus.ksu.edu>
2017-11-06 15:54:07 -0600

    Modifying version 2 of the bispectrum mock code to be a better factored version of the original

--- BiSpecGPU/mockv2/include/bispec.h
+++ BiSpecGPU/mockv2/include/bispec.h
@@ -6,8 +6,8 @@
 
 __constant__ int d_Ngrid[4];
 __constant__ int d_N[1];
-__constant__ float d_binWidth[1];
-__constant__ int d_numBins[1];
+__constant__ float d_d_binWidth[0][1];
+__constant__ int d_d_numBins[0][1];
 __constant__ float d_klim[2];
 
 #if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ > 600
@@ -26,47 +26,90 @@ __device__ double atomicAdd(double* address, double val)
 }
 #endif
 
-__global__ void calcBk(float4 *dk3d, int4 *k, unsigned int *N_tri, double *Bk) {
-    if (blockIdx.x >= blockIdx.y) {
-        __shared__ double Bk_local[4096];
-        int tid_x = threadIdx.x + blockDim.x*blockIdx.x;
-        int tid_y = threadIdx.y + blockDim.y*blockIdx.y;
-        int tid = threadIdx.x + blockDim.x*threadIdx.y;
-        for (int i = tid*4; i < tid*4 + 4; ++i) {
-            Bk_local[i] = 0;
-        }
-        __syncthreads();
-        if (tid_x >= tid_y && tid_x < d_N[0] && tid_y < d_N[0]) {
-            float4 dk_1 = dk3d[k[tid_y].w];
-            float4 dk_2 = dk3d[k[tid_x].w];
-            int4 k_3 = {-k[tid_x].x - k[tid_y].x, -k[tid_x].y - k[tid_y].y, -k[tid_x].z - k[tid_y].z, 0};
-            int i3 = k_3.x + d_Ngrid[0]/2;
-            int j3 = k_3.y + d_Ngrid[1]/2;
-            int k3 = k_3.z + d_Ngrid[2]/2;
+__global__ void calcBk(float4 *dk3d, int4 *k, double *Bk) {
+    int tid = threadIdx.x + blockIdx.x*blockDim.x;
+    
+    int xShift = d_Ngrid[0]/2;
+    int yShift = d_Ngrid[1]/2;
+    int zShift = d_Ngrid[2]/2;
+    
+    __shared__ double Bk_local[4096];
+    for (int i = threadIdx.x*4; i < threadIdx.x*4 + 4; ++i)
+        Bk_local[i] = 0;
+    __syncthreads();
+    
+    if (tid < d_N[0]) {
+        int4 k_1 = k[tid];
+        float4 dk_1 = dk3d[k_1.w];
+        int ik1 = (dk_1.z - d_klim[0])/d_binWidth[0];
+        for (int i = tid; i < d_N[0]; ++i) {
+            int4 k_2 = k[i];
+            float4 dk_2 = dk3d[k_2.w];
+            int4 k_3 = {-k_1.x - k_2.x, -k_1.y - k_2.y, -k_1.z - k_2.z, 0};
+            int i3, j3, k3;
+            i3 = k_3.x + xShift;
+            j3 = k_3.y + yShift;
+            k3 = k_3.z + zShift;
             if (i3 >= 0 && j3 >= 0 && k3 >= 0 && i3 < d_Ngrid[0] && j3 < d_Ngrid[1] && k3 < d_Ngrid[2]) {
                 k_3.w = k3 + d_Ngrid[2]*(j3 + d_Ngrid[1]*i3);
                 float4 dk_3 = dk3d[k_3.w];
                 if (dk_3.z < d_klim[1] && dk_3.z >= d_klim[0]) {
                     float grid_cor = dk_1.w*dk_2.w*dk_3.w;
-                    double val = (dk_1.x*dk_2.x*dk_3.x - dk_1.x*dk_2.y*dk_3.y - dk_1.y*dk_2.x*dk_3.y -
+                    double val = (dk_1.x*dk_2.x*dk_3.x - dk_1.x*dk_2.y*dk_3.y - dk_1.y*dk_2.x*dk_3.y - 
                                   dk_1.y*dk_2.y*dk_3.x)*grid_cor;
-                    int ik1 = (dk_1.z - d_klim[0])/d_binWidth[0];
                     int ik2 = (dk_2.z - d_klim[0])/d_binWidth[0];
                     int ik3 = (dk_3.z - d_klim[0])/d_binWidth[0];
-                    int bin = ik3 + d_numBins[0]*(ik2 + d_numBins[0]*ik1);
+                    int bin = ik3 + d_d_numBins[0][0]*(ik2 + d_d_numBins[0][0]*ik1);
                     atomicAdd(&Bk_local[bin], val);
-                    atomicAdd(&N_tri[bin], 1);
                 }
             }
         }
         __syncthreads();
+        
+        for (int i = threadIdx.x*4; i < threadIdx.x*4 + 4; ++i) {
+            atomicAdd(&Bk[i], Bk_local[i]);
+        }
+    }
+}
+
+__global__ void calcNtri(float4 *dk3d, int4 *k, int4 *k2, unsigned int *N_tri) {
+    int tid = threadIdx.x + blockIdx.x*blockDim.x;
     
-        if (threadIdx.x == 0 && threadIdx.y == 0) {
-            for (int i = 0; i < 4096; ++i) {
-                if (Bk_local[i] != 0) {
-                    atomicAdd(&Bk[i], Bk_local[i]);
+    int xShift = d_Ngrid[0]/2;
+    int yShift = d_Ngrid[1]/2;
+    int zShift = d_Ngrid[2]/2;
+    
+    __shared__ unsigned int Ntri_local[4096];
+    for (int i = threadIdx.x*4; i < threadIdx.x*4 + 4; ++i)
+        Ntri_local[i] = 0;
+    __syncthreads();
+    
+    if (tid < d_N[0]) {
+        int4 k_1 = k1[tid];
+        float4 dk_1 = dk3d[k_1.w];
+        int ik1 = (dk_1.z - d_klim[0])/d_binWidth[0];
+        for (int i = tid; i < d_N[0]; ++i) {
+            int4 k_2 = k2[i];
+            float4 dk_2 = dk3d[k_2.w];
+            int4 k_3 = {-k_1.x - k_2.x, -k_1.y - k_2.y, -k_1.z - k_2.z, 0};
+            int i3 = k_3.x + xShift;
+            int j3 = k_3.y + yShift;
+            int k3 = k_3.z + zShift;
+            if (i3 >= 0 && j3 >= 0 && k3 >= 0 && i3 < d_Ngrid[0] && j3 < d_Ngrid[1] && k3 < d_Ngrid[2]) {
+                k_3.w = k3 + d_Ngrid[2]*(j3 + d_Ngrid[1]*i3);
+                float4 dk_3 = dk3d[k_3.w];
+                if (dk_3.z < d_klim[1] && dk_3.z >= d_klim[0]) {
+                    int ik2 = (dk_2.z - d_klim[0])/d_binWidth[0];
+                    int ik3 = (dk_3.z - d_klim[0])/d_binWidth[0];
+                    int bin = ik3 + d_numBins[0]*(ik2 + d_numBins[0]*ik1);
+                    atomicAdd(&Ntri_local[bin], 1);
                 }
             }
+        }
+        __syncthreads();
+
+        for (int i = threadIdx.x*4; i < threadIdx.x*4 + 4; ++i) {
+            atomicAdd(&N_tri[i], Ntri_local[i]);
         }
     }
 }
